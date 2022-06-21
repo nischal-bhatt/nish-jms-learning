9c386d91dc747f6df652e25fa59593649ae0dbac
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-21 23:50:49 +0800

    committing priority stuff - default priority etc

--- src/main/java/com/nish/basics/jms/messageStructure/MessagePriority.java
+++ src/main/java/com/nish/basics/jms/messageStructure/MessagePriority.java
@@ -3,6 +3,7 @@ package com.nish.basics.jms.messageStructure;
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
 import javax.jms.JMSProducer;
+import javax.jms.Message;
 import javax.jms.Queue;
 import javax.naming.InitialContext;
 
@@ -25,13 +26,13 @@ public class MessagePriority {
 	    	   messages[1] = "message two";
 	    	   messages[2] = "message three";
 	    	   
-	    	   producer.setPriority(3);
+	    	  // producer.setPriority(3);
 	    	   producer.send(queue, messages[0]);
 	    	   
-	    	   producer.setPriority(1);
+	    	//   producer.setPriority(1);
 	    	   producer.send(queue, messages[1]);
 	    	   
-	    	   producer.setPriority(9);
+	    	 //  producer.setPriority(9);
 	    	   producer.send(queue, messages[2]);
 	    	   //message 3 has highest priority of 9
 	    	   
@@ -39,7 +40,9 @@ public class MessagePriority {
 	      
 	           for (int i =0; i<3; i++)
 	           {
-	        	   System.out.println(createConsumer.receiveBody(String.class));
+	        	   Message receivedMessage=createConsumer.receive();
+	        	   System.out.println(receivedMessage.getJMSPriority());
+	        	  // System.out.println(createConsumer.receiveBody(String.class));
 	        	   
 	           }
 	       
