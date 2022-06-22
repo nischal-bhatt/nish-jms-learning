af1b815d09540c48e0224b26e103fbdb79515f6b
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-22 12:46:54 +0800

    creating an object message

---
+++ src/main/java/com/nish/basics/jms/messageStructure/MessageExpirationDemo.java
@@ -0,0 +1,36 @@
+package com.nish.basics.jms.messageStructure;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageExpirationDemo {
+
+	public static void main(String[] args) throws NamingException, InterruptedException {
+		
+		InitialContext context = new InitialContext();
+       Queue queue= (Queue) context.lookup("queue/myQueueNishi");
+       Queue expiryqueue= (Queue) context.lookup("queue/expiryQueue");
+       try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+    		 JMSContext jmscontext = cf.createContext() ) {
+    	   JMSProducer producer = jmscontext.createProducer();
+    	   producer.setTimeToLive(2000);
+    	   producer.send(queue,"hi im nish");
+    	   Thread.sleep(5000);
+    	   Message receiveBody = jmscontext.createConsumer(queue).receive(5000);
+    	   
+    	   System.out.println(receiveBody);
+    	   
+    	   System.out.println(jmscontext.createConsumer(expiryqueue).receiveBody(String.class));
+    	   
+    	   
+       }
+       
+	}
+
+}
---
+++ src/main/java/com/nish/basics/jms/messageStructure/Patient.java
@@ -0,0 +1,37 @@
+package com.nish.basics.jms.messageStructure;
+
+import java.io.Serializable;
+
+public class Patient implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+
+}
